e76825c304cb15c58d34a263663b44c0d66ba281
lbruestl1 <lbruestl1@stud.hs-offenburg.de>
2025-06-04 22:40:17 +0200

    extended entity class

--- Entity.cpp
+++ Entity.cpp
@@ -1,7 +1,8 @@
 #include "Entity.h"
 
-Entity::Entity(float pos_x, float pos_y, sf::Texture& texture) :
-	sprite_(texture)
+Entity::Entity(float pos_x, float pos_y, sf::Texture& texture, int health) :
+	sprite_(texture),
+	health_(health)
 {
 	sprite_.setPosition({ pos_x, pos_y });
 }
@@ -9,6 +10,22 @@ Entity::Entity(float pos_x, float pos_y, sf::Texture& texture) :
 void Entity::render(sf::RenderTarget& target)
 {
 	target.draw(sprite_);
+}
+
+void Entity::move(sf::Vector2f dir)
+{
+	sprite_.move(dir);
+}
+
+void Entity::setRotation(sf::Angle angle)
+{
+	sprite_.setRotation(angle);
+}
+
+bool Entity::damage(int damage)
+{
+	health_ -= damage;
+	return health_ <= 0;
 }
 
 sf::FloatRect Entity::getGlobalBounds() const
--- Entity.h
+++ Entity.h
@@ -5,16 +5,23 @@
 class Entity
 {
 public:
-	Entity(float pos_x, float pos_y, sf::Texture& texture);
+	Entity(float pos_x, float pos_y, sf::Texture& texture, int health);
 	virtual ~Entity() = default;
 
 	virtual void update(float dt) = 0;
 	virtual void render(sf::RenderTarget& target);
+
+	void move(sf::Vector2f dir);
+	void setRotation(sf::Angle angle);
+
+	virtual bool damage(int damage);
 
 	// Getters
 	sf::FloatRect getGlobalBounds() const;
 
 private:
 	sf::Sprite sprite_;
+
+	int health_;
 };
 
