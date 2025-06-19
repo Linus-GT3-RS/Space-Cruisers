1ae4e2836293845473157a136aa7ff46f588ad18
lbruestl1 <lbruestl1@stud.hs-offenburg.de>
2025-06-19 23:35:18 +0200

    implement Animation class

--- Animations/Animation.h
+++ Animations/Animation.h
@@ -1,13 +1,28 @@
 #pragma once
 
+#include "SFML/Graphics.hpp"
+
 class Animation
 {
 public:
-	Animation();
-	virtual ~Animation();
+	Animation(sf::Sprite& sprite, sf::Texture& animSheet,
+			  sf::IntRect startFrame, int frameWidth, int numFrames);
+	virtual ~Animation() = default;
+
+	void update(float dt);
+	void reset();
 
 	friend class AnimationComponent;
 
 private:
+	sf::Sprite& sprite_;
+	sf::Texture& animSheet_;
+
+	sf::IntRect startFrame_;
+	int frameWidth_;
+	int numFrames_;
+	int curFrame_;
+
+	float curTimer_;
 };
 
