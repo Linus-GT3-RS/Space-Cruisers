ccf4768149c95ad27f9d47307d387ce0f8fc0dbd
lbruestl1 <lbruestl1@stud.hs-offenburg.de>
2025-06-22 23:02:07 +0200

    add documentation

--- Animations/Animation.h
+++ Animations/Animation.h
@@ -2,27 +2,56 @@
 
 #include "SFML/Graphics.hpp"
 
+////////////////////////////////////////////////////////////
+/// \brief Handles sprite sheet-based frame animations for a sprite.
+/// Think of a sprite as a point that moves around, with a rectangular frame from a texture (sprite sheet) drawn on top.
+/// By changing which frame is displayed over time, the sprite appears animated.
+///
+/// The Animation class manages a sequence of frames from a texture (sprite sheet),
+/// updating the displayed frame based on a timer and looping through frames in a single row in the texture 1 by 1.
+/// It supports multiple animations per texture by keeping the y-coordinate fixed.
+////////////////////////////////////////////////////////////
 class Animation
 {
 public:
+	////////////////////////////////////////////////////////////
+	/// \brief Constructs an Animation object.
+	///
+	/// \param sprite Reference to the sprite to animate.
+	/// \param animSheet Reference to the texture containing animation frames.
+	/// \param startFrame Rectangle specifying the first frame in the row.
+	/// \param frameWidth Width of each animation frame.
+	/// \param numFrames Number of frames in the animation row.
+	////////////////////////////////////////////////////////////
 	Animation(sf::Sprite& sprite, sf::Texture& animSheet,
-			  sf::IntRect startFrame, int frameWidth, int numFrames);
+     sf::IntRect startFrame, int frameWidth, int numFrames);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Virtual destructor.
+	////////////////////////////////////////////////////////////
 	virtual ~Animation() = default;
 
+	////////////////////////////////////////////////////////////
+	/// \brief Updates the animation, advancing the frame if timer is reached
+	///
+	/// \param dt Delta time
+	////////////////////////////////////////////////////////////
 	void update(float dt);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Resets the animation to the first frame.
+	////////////////////////////////////////////////////////////
 	void reset();
 
 	friend class AnimationComponent;
 
 private:
-	sf::Sprite& sprite_;
-	sf::Texture& animSheet_;
+	sf::Sprite& sprite_;      //!< Reference to the sprite being animated
+	sf::Texture& animSheet_;  //!< Reference to the texture containing animation frames
 
-	sf::IntRect startFrame_;
-	int frameWidth_;
-	int numFrames_;
-	int curFrame_;
-
-	float curTimer_;
+	sf::IntRect startFrame_;  //!< Rectangle of the first frame in the animation row
+	int frameWidth_;          //!< Width of each frame in pixels
+	int numFrames_;           //!< Total number of frames in the animation
+	int curFrame_;            //!< Index of the current frame
+	float curTimer_;          //!< Timer for frame switching
 };
-
--- Animations/AnimationComponent.h
+++ Animations/AnimationComponent.h
@@ -4,18 +4,52 @@
 #include <map>
 #include "AnimationTypes.h"
 
+////////////////////////////////////////////////////////////
+/// \brief Manages multiple animations and controls their playback.
+///
+/// The AnimationComponent class stores and manages a collection of Animation objects,
+/// allowing switching between them and updating the currently active animation.
+////////////////////////////////////////////////////////////
 class AnimationComponent
 {
 public:
+	////////////////////////////////////////////////////////////
+	/// \brief Constructor.
+	////////////////////////////////////////////////////////////
 	AnimationComponent();
+
+	////////////////////////////////////////////////////////////
+	/// \brief Destructor. Cleans up all stored Animation pointers.
+	////////////////////////////////////////////////////////////
 	virtual ~AnimationComponent();
 
+	////////////////////////////////////////////////////////////
+	/// \brief Adds an animation to the component.
+	///
+	/// \param key The identifier for the animation.
+	/// \param pAnim Pointer to the Animation object to add.
+	////////////////////////////////////////////////////////////
 	void addAnimation(AnimationType key, Animation* pAnim);
 
+	////////////////////////////////////////////////////////////
+	/// \brief Plays the animation associated with the given key.
+	///
+	/// If a different animation is played than the previous call, the current animation is reset and played from start.
+	/// The current animation's update function is called with the provided delta time.
+	///
+	/// \param key The identifier for the animation to play.
+	/// \param dt  The delta time (time since last update), used for animation timing.
+	////////////////////////////////////////////////////////////
 	void playAnimation(AnimationType key, const float dt);
 
 private:
+	////////////////////////////////////////////////////////////
+	/// \brief Map storing all animations by their type.
+	////////////////////////////////////////////////////////////
 	std::map<AnimationType, Animation*> animations_;
+
+	////////////////////////////////////////////////////////////
+	/// \brief The type of the last animation that was played.
+	////////////////////////////////////////////////////////////
 	AnimationType lastAnimPlayed_;
 };
-
